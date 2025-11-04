28bdfdfd426f0c712921a5be2abe28175d851652
Aaron Morey <aaronmorey@gmail.com>
2025-11-04 08:36:18 -0600

    Fix shadowed variables Rename variables for clarity Convert raw lists to np arrays for consistency

--- python/drafting/2026_draft_odds.py
+++ python/drafting/2026_draft_odds.py
@@ -10,10 +10,10 @@ import pprint
 
     A script to estimate the Cardinals' 2026 MLB Draft position odds by
     simulating the draft.
-                                                           
+
 """
 
-NUM_SIMULATIONS = 1_000_000
+NUM_SIMULATIONS = 100_000 # 1_000_000 # 5_000_000
 
 
 def set_up_data():
@@ -40,20 +40,20 @@ def set_up_data():
         Nationals (.407) -- ineligible
         Angels (.444) -- ineligible
     """
-    record_order = [
+    record_order = np.array([
         "COL", "CHW", "WAS", "MIN", "PIT", "LAA",
         "BAL", "ATH", "ATL", "TBR", "STL", "MIA",
         "ARI", "TEX", "SFG", "KCR", "NYM", "HOU",
-    ]
+    ])
 
-    ineligible = [ "COL", "WAS", "LAA", ]
+    ineligible = np.array([ "COL", "WAS", "LAA", ])
 
     lottery = np.concatenate(
         [np.full(2773, "CHW"), np.full(2118, "MIN"), np.full(1681, "PIT"),
-            np.full(924, "BAL"), np.full(655, "ATH"), np.full(454, "ATL"),
-            np.full(303, "TBR"), np.full(235, "STL"), np.full(185, "MIA"),
-            np.full(151, "ARI"), np.full(134, "TEX"), np.full(101, "SFG"),
-            np.full(84, "KCR"), np.full(67, "NYM"), np.full(34, "HOU"),]
+         np.full(924, "BAL"), np.full(655, "ATH"), np.full(454, "ATL"),
+         np.full(303, "TBR"), np.full(235, "STL"), np.full(185, "MIA"),
+         np.full(151, "ARI"), np.full(134, "TEX"), np.full(101, "SFG"),
+         np.full(84, "KCR"), np.full(67, "NYM"), np.full(34, "HOU"),]
     )
 
     return lottery, record_order, ineligible
@@ -75,17 +75,17 @@ def draft(lottery, record_order, ineligible):
                attempted to reproduce that logic, I'm just taking MLB's
                word for it. They should know.
     """
-    draft_order = []
+    draft_order = np.array([])
 
     # the lottery for the first six spots
     for _ in range(6):
-        lottery, record_order = pick(draft_order, lottery, record_order)
+        lottery, record_order, draft_order = pick(draft_order, lottery, record_order)
 
     # Round out the top-9, since ineligible teams aren't allowed to pick under 10
     eligible_top_nine_teams_picked = 0
     for team in record_order:
         if team not in ineligible:
-            lottery, record_order = pick(draft_order, lottery, record_order)
+            lottery, record_order, draft_order = pick(draft_order, lottery, record_order)
             eligible_top_nine_teams_picked += 1
 
         if eligible_top_nine_teams_picked > 2:
@@ -97,14 +97,13 @@ def draft(lottery, record_order, ineligible):
 
 def pick(draft_order, lottery, record_order):
     choice = np.random.choice(lottery)
-    draft_order.append(choice)
+    draft_order = np.append(draft_order, choice)
     lottery = lottery[lottery != choice]
     record_order = record_order[record_order != choice]
-    return lottery, record_order
+    return lottery, record_order, draft_order
 
-
-if __name__ == "__main__":
-    cardinals_draft_spots = []
+def main():
+    cardinals_draft_spots = np.array([])
     lottery, record_order, ineligible_teams = set_up_data()
 
     for _ in range(NUM_SIMULATIONS):
@@ -113,15 +112,19 @@ if __name__ == "__main__":
         record_copy = np.copy(record_order)
         order = draft(lottery_copy, record_copy, ineligible_teams)
         cardinals_spot = np.where(order == "STL")[0]
-        cardinals_draft_spots.append(cardinals_spot + 1)
+        # Don't forget, arrays are zero-indexed but lotteries aren't!
+        cardinals_draft_spots = np.append(cardinals_draft_spots, cardinals_spot + 1)
 
     cardinals_draft_spots = np.array(cardinals_draft_spots)
-    values, counts = np.unique(cardinals_draft_spots, return_counts=True)
-    raw_result = {int(k): int(v) for k, v in zip(values, counts)}
-    percent_result = {}
-    
-    for key in raw_result:
-        percentage = (raw_result[key] / NUM_SIMULATIONS)
-        percent_result[key] = f'{percentage:.2%}'
+    draft_spot_values, draft_spot_counts = np.unique(cardinals_draft_spots, return_counts=True)
+    raw_draft_result = {int(k): int(v) for k, v in zip(draft_spot_values, draft_spot_counts)}
+    percent_draft_result = {}
 
-    pprint.pprint(percent_result)
+    for key in raw_draft_result:
+        percentage = raw_draft_result[key] / NUM_SIMULATIONS
+        percent_draft_result[key] = f"{percentage:.5%}"
+
+    pprint.pprint(percent_draft_result)
+
+if __name__ == "__main__":
+    main()
