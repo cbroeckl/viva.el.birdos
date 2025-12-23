055e0ef49aad2c554bbcbfca090161c7186dff3f
Corey Broeckling <4959997+cbroeckl@users.noreply.github.com>
2025-12-22 17:06:04 -0700

    Update get.minors.data.R

--- cdb/minor.league.comp.batter/get.minors.data.R
+++ cdb/minor.league.comp.batter/get.minors.data.R
@@ -1,9 +1,9 @@
 library(baseballr)
 library(tidyverse)
 
-This function requires both a start date and an end date, written in the format "YYYY-MM-DD". 
+# This function requires both a start date and an end date, written in the format "YYYY-MM-DD". 
 # By default, it only returns Triple-A data. To get data from other levels, set the level 
-# argument to one of the following: "milb", "aa", "higha", "a", "lowa", or "rookie".
+# argument to one of the following: "aaa", "aa", "higha", "a", "lowa"
 # from: https://github.com/BillPetti/baseballr/issues/257
 
 bref_daily_batter_milb <- function(t1, t2, level = "aaa") {
@@ -26,7 +26,7 @@ bref_daily_batter_milb <- function(t1, t2, level = "aaa") {
   df$season <- as.integer(substr(t1, 1, 4))
   df$uBB <- with(df, BB-IBB)
   
-  df <- df[,c(29, 1:10, 28, 11:16, 30, 17:27)]
+  # df <- df[,c(29, 1:10, 28, 11:16, 30, 17:27)]  # this seems to break things, sometimes.
   df$Team <- gsub(" $", "", df$Team, perl=T)
   df <- df %>% 
     dplyr::filter(.data$Name != "Name")
@@ -46,5 +46,32 @@ bref_daily_batter_milb <- function(t1, t2, level = "aaa") {
   return(df)
 }
 
-d <- bref_daily_batter_milb(t1 = "2025-05-01", t2 = "2025-05-31", level = "higha")
+yrs <- c(2000:2019, 2021:2025)
+levs <- c("aaa", "aa", "higha", "a", "lowa", "rk")
+stats.list <- list(length = 0)
+for(yr in yrs) {
+  for(lev in levs) {
+    dt <- paste0(yr, c("-1-01", "-12-31"))
+    d <- tryCatch(
+      #this is the chunk of code we want to run
+      {bref_daily_batter_milb(t1 = dt[1], t2 = dt[2], level = lev)
+        #when it throws an error, the following block catches the error
+      }, error = function(msg){
+        return(NA)
+      }
+    )
+    
+    if(is.data.frame(d)) {
+      d <- data.frame(
+        "year" = rep(yr, nrow(d)),
+        "level" = rep(lev, nrow(d)),
+        d
+      )
+      stats.list[[length(stats.list)+1]] <- d
+    }
+    rm(d)
+  }
+}
+
+save(stats.list, file = )
 
